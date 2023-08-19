f1f64428394f4bd64439bf8b0fb0365334197cde
mohammadgdz <mhd.gdz@gmail.com>
2023-08-19 12:58:54 +0330

    Initial Commit

---
+++ Async Image/Async_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_ImageApp.swift
+//  Async Image
+//
+//  Created by Mohammad on 2023-08-19.
+//
+
+import SwiftUI
+
+@main
+struct Async_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
